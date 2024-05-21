488ef9254796d91e92d627da1dea98d0d2cc5163
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-05-21 08:04:25 -0300

    Primeros cambios en la url

--- ProntaEntregaDjango/ProntaEntregaApp/views.py
+++ ProntaEntregaDjango/ProntaEntregaApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import generics, status, exceptions
+from rest_framework.response import Response
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 from django.contrib import  messages
@@ -13,4 +14,8 @@ from django.shortcuts import render
 
 class loginView(generics.GenericAPIView):
     def get(self, request: HttpRequest, *args, **kwargs):
+        return Response ({"message": "Hello, world!"})
+    
+class userView(generics.GenericAPIView):
+    def get(request, pk):
         return Response ({"message": "Hello, world!"})
