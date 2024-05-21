8e18d4441aca23861bd6cea7618db9ba601b603a
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-05-21 08:03:53 -0300

    Primeros cambios de prueba en el views

--- ProntaEntregaDjango/ProntaEntregaApp/views.py
+++ ProntaEntregaDjango/ProntaEntregaApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from rest_framework import generics, status, exceptions
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 from django.contrib import  messages
@@ -10,6 +11,6 @@ from ProntaEntregaApp.models import *
 
 from django.shortcuts import render
 
-class loginView(generics.GenericApiView):
+class loginView(generics.GenericAPIView):
     def get(self, request: HttpRequest, *args, **kwargs):
         return Response ({"message": "Hello, world!"})
