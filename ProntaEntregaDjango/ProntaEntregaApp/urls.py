8834d439bdb6fb3294e582d3837701cac6f5fed3
Lucio Peralta <lucio.peralta.ar@gmail.com>
2024-05-28 10:39:26 -0300

    Creacion de clase para visualizar Stock y Usuarios

--- ProntaEntregaDjango/ProntaEntregaApp/urls.py
+++ ProntaEntregaDjango/ProntaEntregaApp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', UserLogin.as_view(), name='user_login'),
     path('test/', TestToken.as_view(), name='test_token'),
     path('cambiar_contrasenia/', CambiarContrasenia.as_view(), name='change_password'),
+    path('stock/<int:categoria_id>/', VerStockYProducto.as_view(), name='ver_stock_producto'),
+    path('ver_usuarios/', VerUsuarios.as_view(), name='ver_usuarios'),
 ]
--- ProntaEntregaDjango/ProntaEntregaApp/views.py
+++ ProntaEntregaDjango/ProntaEntregaApp/views.py
@@ -17,6 +17,7 @@ from rest_framework.authtoken.models import Token
 from ProntaEntregaApp.serializers.userSerializers import *
 from ProntaEntregaApp.models import Usuario
 from ProntaEntregaApp.validations import *
+from django.http import JsonResponse
 
 
 
@@ -69,7 +70,6 @@ class CambiarContrasenia(APIView):
     def post(self, request):
         user = request.user
 
-        # Verificar que los campos necesarios están presentes en la solicitud
         if 'old_password' not in request.data or 'new_password' not in request.data or 'new_password_repeat' not in request.data:
             return Response({'error': 'Por favor, proporcione la contraseña antigua, la nueva contraseña y la repetición de la nueva contraseña.'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -77,17 +77,62 @@ class CambiarContrasenia(APIView):
         new_password = request.data['new_password']
         new_password_repeat = request.data['new_password_repeat']
 
-        # Verificar que la contraseña antigua es correcta
         if not user.check_password(old_password):
             return Response({'error': 'La contraseña antigua es incorrecta.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Verificar que la nueva contraseña se ha introducido correctamente dos veces
         if new_password != new_password_repeat:
             return Response({'error': 'Las nuevas contraseñas no coinciden.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Cambiar la contraseña del usuario
         user.set_password(new_password)
         user.save()
 
         return Response({'success': 'La contraseña ha sido cambiada con éxito.'}, status=status.HTTP_200_OK)
     
+
+class VerStockYProducto(APIView):
+    def get(self, request, categoria_id):
+        productos_de_categoria = Producto.objects.filter(id_categoriaproducto=categoria_id)
+        productos_con_stock = []
+
+        for producto in productos_de_categoria:
+            detalle_stock = Detallestockproducto.objects.filter(id_producto=producto.id_producto).first()
+            cantidad_stock = detalle_stock.cantidad if detalle_stock else 0
+
+            producto_con_stock = {
+                'nombre_producto': producto.nombre,
+                'descripcion': producto.descripcion,
+                'stock_disponible': cantidad_stock
+            }
+            productos_con_stock.append(producto_con_stock)
+        
+        return JsonResponse(productos_con_stock, safe=False)
+
+
+class VerUsuarios(APIView):
+    def get(self, request):
+        # Obtener todos los usuarios de la base de datos
+        usuarios = Usuario.objects.all()
+
+        # Serializar los usuarios a JSON
+        usuarios_json = []
+        for usuario in usuarios:
+            usuario_json = {
+                'id': usuario.id_usuario,
+                'nombre': usuario.nombre,
+                'apellido': usuario.apellido,
+                'nombre_usuario': usuario.nombreusuario,
+                'documento': usuario.documento,
+                'telefono': usuario.telefono,
+                'email': usuario.email,
+                'genero': usuario.genero,
+                'fecha_union': usuario.fechaUnion,
+                'last_login': usuario.last_login,
+                'is_superuser': usuario.is_superuser,
+                'id_direccion': usuario.id_direccion_id,
+                'id_tipo_usuario': usuario.id_tipousuario_id,
+                'id_tipo_documento': usuario.id_tipodocumento_id
+            }
+            usuarios_json.append(usuario_json)
+
+        # Devolver la respuesta como JSON
+        return JsonResponse(usuarios_json, safe=False)
