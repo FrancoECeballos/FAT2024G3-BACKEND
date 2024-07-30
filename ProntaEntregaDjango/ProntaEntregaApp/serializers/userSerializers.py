63d48c9a8421b1e7330b9fcd7ecfe694bd003468
Chialvo <lautarochialvo2005@gmail.com>
2024-07-30 09:29:48 -0300

    casa y usuario con img

--- ProntaEntregaDjango/ProntaEntregaApp/serializers/userSerializers.py
+++ ProntaEntregaDjango/ProntaEntregaApp/serializers/userSerializers.py
@@ -48,7 +48,7 @@ class UsuarioUpdateSerializer(serializers.ModelSerializer):
 class UsuarioRegistroSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUsuario
-        fields = ['nombre', 'apellido', 'nombreusuario', 'password', 'documento', 'telefono', 'email', 'genero', 'id_direccion', 'id_tipousuario', 'id_tipodocumento']
+        fields = ['nombre', 'apellido', 'nombreusuario', 'password', 'documento', 'telefono', 'email', 'genero', 'imagen', 'id_direccion', 'id_tipousuario', 'id_tipodocumento']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -80,7 +80,7 @@ class UsuarioRegistroSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             genero=validated_data['genero'],
             password=validated_data['password'],
-            imagen=None,
+            imagen=validated_data['imagen'],
             id_direccion=validated_data['id_direccion'],
             id_tipousuario=validated_data['id_tipousuario'],
             id_tipodocumento=validated_data['id_tipodocumento'],
