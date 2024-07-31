3ad81f634ecf20f66ddb594eda49077bff6bf72a
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-07-31 11:54:01 -0300

    FIxed error in DetalleStockProducto inserts

--- ProntaEntregaDjango/PRONTAENTREGADB.sql
+++ ProntaEntregaDjango/PRONTAENTREGADB.sql
@@ -346,10 +346,10 @@ INSERT INTO Transporte (marca, modelo, patente, kilometraje, estadoITV, anio, id
 
 insert into DetalleStockProducto (cantidad, cantidadUnidades, id_stock, id_producto, id_unidadMedida) values
 	(100, 1, 1, 2, 1),
-    (200, 1, 3, 3, 2),
+    (200, 1, 3, 3, 1),
     (300, 1, 2, 1, 3),
-	(430, 1, 1, 1, 1),
-    (22, 1, 3, 2, 2),
+	(430, 1, 1, 1, 3),
+    (22, 1, 3, 2, 1),
     (120, 1, 2, 3, 3);
     
 insert into AportePedido (descripcion,cantidad,id_pedido) values 
