1c292002b2f679238e44c04907947369d35c940e
Chialvo <lautarochialvo2005@gmail.com>
2024-08-07 09:00:08 -0300

    fix

--- ProntaEntregaDjango/ProntaEntregaApp/serializers/generalSerializers.py
+++ ProntaEntregaDjango/ProntaEntregaApp/serializers/generalSerializers.py
@@ -79,7 +79,7 @@ class ObraSerializer(serializers.ModelSerializer):
     
 class DetalleobrausuarioSerializer(serializers.ModelSerializer):
     id_obra = serializers.PrimaryKeyRelatedField(queryset=Obra.objects.all())
-    id_tipoUsuario = serializers.PrimaryKeyRelatedField(queryset=Tipousuario.objects.all())
+    id_tipousuario = serializers.PrimaryKeyRelatedField(queryset=Tipousuario.objects.all())
     class Meta:
         model = Detalleobrausuario
         fields = '__all__'
