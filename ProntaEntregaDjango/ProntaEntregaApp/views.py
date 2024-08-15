3f2990c6f6e0b6a5bb5dac62b8bf269d9d427563
David <davidandradag@gmail.com>
2024-08-15 10:53:27 -0300

    cambiao GetDetallestockproducto_Total

--- ProntaEntregaDjango/ProntaEntregaApp/views.py
+++ ProntaEntregaDjango/ProntaEntregaApp/views.py
@@ -1037,19 +1037,38 @@ class PostDetallestockproducto(APIView):
         request.data.update({'fecha_creacion':datetime.today().date()})
         serializer = CrearDetallestockproductoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save() 
+            
 
             dd = timedelta(days=7)
             d = datetime.today().date() - dd
-            print(d)
+            print(request.data['id_producto'])
+            print(request.data['id_stock'])
             crear_checkpoint = True
-            for x in Detallestockproducto.objects.filter(checkpoint = True):
+            for x in Detallestockproducto.objects.filter(id_stock = request.data['id_stock'],id_producto = request.data['id_producto'],checkpoint = True):
                 if x.fecha_creacion >= d:
                     print(x)
                     crear_checkpoint = False
             
             if crear_checkpoint == True:
-                print("*crea checkpoint*")
+
+                detalle = Detallestockproducto.objects.filter(id_stock=request.data['id_stock'], id_producto=request.data['id_producto'],)
+                total = 0
+                for x in detalle:
+                    total = total + x.cantidad
+
+                Detallestockproducto.objects.create(
+                checkpoint=True,
+                fecha_creacion=datetime.today().date(),
+                cantidad=total,
+                id_producto_id=request.data['id_producto'],
+                id_stock_id=request.data['id_stock']
+                )
+                print('se creo checkpoint')
+            else:
+                print('no se necesita crear checkpoint')
+            
+            
+            serializer.save() ## esto tiene que quedar al despues de todo el resto
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -1313,11 +1332,23 @@ class GetDetallestockproducto_Total(APIView):
 
     def get(self, request, id_stock,id_producto):
         try:
-            detalle = Detallestockproducto.objects.filter(id_stock=id_stock, id_producto=id_producto)
-            total = 0
+            
+
+            ultimo_checkpoint = Detallestockproducto.objects.filter(id_stock=id_stock, id_producto=id_producto,checkpoint= True)
+            print(ultimo_checkpoint)
+            ultimo_checkpoint = ultimo_checkpoint.order_by('fecha_creacion').first()
+            print(ultimo_checkpoint)
+        
+            try:
+                detalle = Detallestockproducto.objects.filter(id_stock=id_stock, id_producto=id_producto, fecha_creacion__gte = ultimo_checkpoint.fecha_creacion)
+                total = ultimo_checkpoint.cantidad
+            except AttributeError:
+                print('hubo errores')
+                detalle = Detallestockproducto.objects.filter(id_stock=id_stock, id_producto=id_producto)
+                total = 0
+
             for x in detalle:
                 total = total + x.cantidad
-            print(total)
             return Response({'total':total}, status=status.HTTP_200_OK)
         except Detallestockproducto.DoesNotExist:
             return Response({'error': 'DetalleStockProducto no encontrado.'}, status=status.HTTP_404_NOT_FOUND)
