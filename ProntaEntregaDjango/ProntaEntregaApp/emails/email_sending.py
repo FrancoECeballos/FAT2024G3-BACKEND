a6af0e6ba48debb55c1ed4a0a70c7857bc78055c
David <davidandradag@gmail.com>
2024-09-03 09:22:50 -0300

    cambios minimos

--- ProntaEntregaDjango/ProntaEntregaApp/emails/email_sending.py
+++ ProntaEntregaDjango/ProntaEntregaApp/emails/email_sending.py
@@ -47,11 +47,7 @@ def verificar_register(email_usuario,nombre,link):
     send(email_usuario,file_contents)
     
 def send(email_usuario,file_contents):
-    mail.send(receiver=email_usuario,  # Email From Any service Provider
-            no_reply='noreplay@example.com', # Redirect receiver to another email when try to reply.
+    mail.send(receiver=email_usuario,
+            no_reply='noreplay@example.com',
             subject='verificar cuenta',
             message=file_contents)
-
-##cambiar_contra('davidandradag@gmail.com','pedrito')
-
-#verificar_register('davidandradag@gmail.com','pedrito')
