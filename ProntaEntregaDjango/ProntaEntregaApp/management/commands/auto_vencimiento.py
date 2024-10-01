6016dd49cdc2657555e50088e10b28b413fa348f
david <davidandradag@gmail.com>
2024-10-01 08:38:03 -0300

    hecho comando y actualizado auto.sh para finalizar pedidos vencidos

---
+++ ProntaEntregaDjango/ProntaEntregaApp/management/commands/auto_vencimiento.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from ProntaEntregaApp.models import *
+from ProntaEntregaApp.serializers.stockSerializers import *
+
+class Command(BaseCommand):
+    help = 'pasa los pedidos disponibles a vencidos si pasa su rango de tiempo establecido'
+
+    def handle(self, *args, **kwargs):
+        estado_pendiente = Estadopedido.objects.get(pk=1)
+        estado_finalizado = Estadopedido.objects.get(pk=3)
+
+        pedidos_dispo = Pedido.objects.filter(id_estadoPedido = estado_pendiente)
+        print("pedidos:")
+        print(pedidos_dispo)
+        print('----')
+
+        for p in pedidos_dispo:
+            print(p.fechavencimiento)
+            print(timezone.now().date())
+
+            if p.fechavencimiento < timezone.now().date():
+                p.id_estadoPedido = estado_finalizado
+                p.save()
+                print('a')
+            else:
+                print('b')
