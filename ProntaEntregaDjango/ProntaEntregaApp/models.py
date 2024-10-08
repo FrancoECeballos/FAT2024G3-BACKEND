6b2d618614ae2c8332e8b64c56785f2705737a89
david <davidandradag@gmail.com>
2024-10-08 09:53:05 -0300

    cambio diminuto a la base de datos

--- ProntaEntregaDjango/ProntaEntregaApp/models.py
+++ ProntaEntregaDjango/ProntaEntregaApp/models.py
@@ -343,7 +343,7 @@ class Estadopedido(models.Model):
 class AportePedido(models.Model):
     id_aportePedido = models.AutoField(db_column='id_aportePedido', primary_key=True)  # Field name made lowercase.
     descripcion = models.CharField(max_length=255, blank=True, null=True)
-    cantidad = models.IntegerField(blank=True, null=True)
+    cantidad = models.IntegerField(blank = False, Null= False)
     fechaAportado = models.DateField(db_column='fechaAportado', blank=True, null=True)
     id_pedido = models.ForeignKey('Pedido', models.DO_NOTHING, db_column='id_pedido', blank=True, null=True)
     id_obra = models.ForeignKey('Obra', models.DO_NOTHING, db_column='id_obra', blank=True, null=True)
@@ -388,7 +388,7 @@ class Estadooferta(models.Model):
 class AporteOferta(models.Model):
     id_aporteOferta = models.AutoField(db_column='id_aporteOferta', primary_key=True)  # Field name made lowercase.
     descripcion = models.CharField(max_length=255, blank=True, null=True)
-    cantidad = models.IntegerField(blank=True, null=True)
+    cantidad = models.IntegerField(blank=False, null=False)
     fechaAportado = models.DateField(db_column='fechaAportado', blank=True, null=True)
     id_oferta = models.ForeignKey('Oferta', models.DO_NOTHING, db_column='id_oferta', blank=True, null=True)
     id_obra = models.ForeignKey('Obra', models.DO_NOTHING, db_column='id_obra', blank=True, null=True)
