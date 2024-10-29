ff9a4ab49eb346a596e4648f47ee13b9f1aa6bbd
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-10-29 11:16:47 -0300

    Small changes to Pedido and Oferta serializers

--- ProntaEntregaDjango/ProntaEntregaApp/serializers/offerSerializers.py
+++ ProntaEntregaDjango/ProntaEntregaApp/serializers/offerSerializers.py
@@ -12,8 +12,8 @@ class EstadoofertaSerializer(serializers.ModelSerializer):
 class OfertaSerializer(serializers.ModelSerializer):
     id_obra = ObraSerializer()
     id_usuario = UsuarioSerializer()
-    id_estadoOferta = EstadoofertaSerializer()
     id_producto = ProductoSerializer()
+    id_estadoOferta = EstadoofertaSerializer()
 
     class Meta:
         model = Oferta
