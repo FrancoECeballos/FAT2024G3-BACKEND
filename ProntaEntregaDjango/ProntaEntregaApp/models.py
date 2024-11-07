393487cb463f78c2150531a792a3a9935e706f3c
Lucio Peralta <lucio.peralta.ar@gmail.com>
2024-11-07 09:19:15 -0300

    Fix creating 0km vehicles

--- ProntaEntregaDjango/ProntaEntregaApp/models.py
+++ ProntaEntregaDjango/ProntaEntregaApp/models.py
@@ -398,7 +398,7 @@ class AporteOferta(models.Model):
 
 
 class TransporteManager(models.Manager): 
-    def crear_transporte(self, marca, modelo, patente, kilometraje, imagen):
+    def crear_transporte(self, marca, modelo, patente, kilometraje, imagen, necesita_mantenimiento=False, descripcion_mantenimiento=''):
         if not marca:
             raise ValueError('El transporte debe tener una marca')
         if not modelo:
@@ -415,7 +415,7 @@ class TransporteManager(models.Manager):
             marca=marca,
             modelo=modelo,
             patente=patente,
-            kilometraje=kilometraje,
+            kilometraje=kilometraje
         )
         transporte.save(using=self._db)
         return transporte
