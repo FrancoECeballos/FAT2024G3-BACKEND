09636b1367b5547a5d3e1227f9f78d1c435ccfce
Lucio Peralta <lucio.peralta.ar@gmail.com>
2024-11-14 09:41:48 -0300

    Allowed to conect with front

--- ProntaEntregaDjango/config/settings.py
+++ ProntaEntregaDjango/config/settings.py
@@ -96,6 +96,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
+    "https://fat2024g3-frontend.onrender.com",
 ]
 
 CORS_ALLOW_METHODS = (
